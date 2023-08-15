ee15a9edcc8a2f054cbdbb89758dd4ec7124315b
TakahiroFukui <130196403+TakahiroFukui@users.noreply.github.com>
2023-08-15 11:54:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -48,21 +48,6 @@ st.title("論文要約アプリ")
 st.write("論文をアップロードしてください")
 
 uploaded_file = st.file_uploader("ファイルを選択してください", type=['pdf'])
-    
-if uploaded_file:
-    pdf_reader = PdfReader(uploaded_file)
-    text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
-    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    model_name=st.session_state.emb_model_name,
-            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
-            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
-            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
-        chunk_size=250,
-        chunk_overlap=0,
-        )
-    return text_splitter.split_text(text)
-else:
-    return None
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
