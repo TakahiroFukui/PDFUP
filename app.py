239138387f53fb3bba807e5e43da7a74312949a5
TakahiroFukui <130196403+TakahiroFukui@users.noreply.github.com>
2023-08-15 15:09:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ system_prompt = """
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": system_prompt}
+        {"role": "system", "content": system_prompt, text}
         ]
 
 # チャットボットとやりとりする関数
